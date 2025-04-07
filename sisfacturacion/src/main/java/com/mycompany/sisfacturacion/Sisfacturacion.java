664303b50402a4c189499abf322e199e95ed36ff
OPASOP112 <rodrigoalf109@gmail.com>
2025-04-07 16:45:42 -0500

    primer commit

---
+++ sisfacturacion/src/main/java/com/mycompany/sisfacturacion/Sisfacturacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sisfacturacion;
+
+/**
+ *
+ * @author ALUMNO
+ */
+public class Sisfacturacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
